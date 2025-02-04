2d51e41ebedaea83e4de2f9283cb90b4844aba12
parithusnavis <57772153+parithusnavis@users.noreply.github.com>
2025-02-04 18:23:44 -0500

    Update streamlit_app.py
    Adding Pandas library to make use of Loc fucntion

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,6 +20,7 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 #st.stop()
 
 # Convert the snowpark dataframe to pandas dataframe so we can us ethe LOC function
+pd_df=my_dataframe.to_pandas()
 st.dataframe(pd_df)
 srt.stop()
 
